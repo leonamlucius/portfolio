2dd2af33b2daa99013ce239e54820a785e667dc7
Leonam L. <leonam@253gmail.com>
2026-03-07 14:06:54 -0300

    -mudança no texto principal

--- src/principal.tsx
+++ src/principal.tsx
@@ -68,11 +68,12 @@ function Principal() {
         </div>
 
         <div className="w-full h-auto flex items-start justify-start p-3 box-border antialiased font-robot text-xl indent-3 pt-6 hover:text-[20.5px] transition-all duration-300 cursor-pointer">
-          Formado em Análise e Desenvolvimento de Sistemas, tenho experiência em
+          <p className="m-0 font-medium">Formado em Análise e Desenvolvimento de Sistemas, tenho experiência em
           desenvolvimento de software, com foco em JavaScript, TypeScript, React, porém,
           com experiência em Spring(Java) e FastAPI(Python). Sou apaixonado por criar soluções eficientes e
           inovadoras, sempre buscando aprimorar minhas habilidades e contribuir
-          para projetos desafiadores.
+          para projetos desafiadores. Tenho experiência no ramo de comércio exterior, porém, sempre busco desenvolver soluções paraa todos os segmentos, com foco em qualidade e eficiência. Estou sempre aberto a novas oportunidades e desafios, buscando crescer profissionalmente e contribuir para o sucesso de projetos e equipes.
+          </p>
         </div>
 
         <div className="w-full h-auto flex flex-col items-center justify-start p-3 box-border antialiased font-robot text-xl">
@@ -80,15 +81,14 @@ function Principal() {
             Minhas stacks
           </div>
 
-          <div className="w-full h-auto gap-2 flex items-start justify-start antialiased font-robot text-xl indent-3 pt-6" id="divIcons">
-            <SiJavascript className="text-4xl text-yellow-400" />
+          <div className="w-full h-auto gap-3 flex items-start justify-start antialiased font-robot text-xl indent-3 pt-6" id="divIcons">
+            <SiJavascript className="text-4xl text-yellow-400 " />
             <SiTypescript className="text-4xl text-blue-600" />
             <SiReact className="text-4xl text-cyan-400" />
             <SiSpring className="text-4xl text-green-600" />
             <SiPython className="text-4xl text-yellow-500" />
             <SiMysql className="text-4xl text-blue-500" />
             <SiFastapi className="text-4xl text-green-500" />
-            
           </div>
         </div>
       </div>
